d74cb456fd258d846ec9932a2eed8bd024c23f83
Chad Peters <capeters@lincoln.ne.gov>
2023-04-11 15:55:01 -0500

    first commit

---
+++ Stage2/Week1/LandingPage.js
@@ -0,0 +1,6 @@
+function ShowLogin() {
+    console.log(document.getElementById("divLoginArea").style.display);
+    document.getElementById("divLoginArea").style.display = "block";
+    document.getElementById("btnLogin").style.display = "none";
+}
+
---
+++ Stage2/survey.js
@@ -0,0 +1,5 @@
+function submitForm() {
+    var newname = document.getElementById("inpName").value;
+    console.log(newname);
+    alert ("Your favorite food survey was received!  Thank you " + newname + "!");
+}
